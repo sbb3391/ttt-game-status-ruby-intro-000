4083b5371585090446f06d26cd6b43b20793bf1a
Sam Bequette <sbb3391@gmail.com>
2020-04-07 01:22:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,9 +22,9 @@ def won?(board)
     if position_taken?(board, winner_set[0]) &&
        board[winner_set[0]] == board[winner_set[1]] &&
        board[winner_set[0]] == board[winner_set[2]]
-       return winner_set
+        winner_set
      else
-      return false
+       false
     end
   end
 end
